b28e9503a51ad0bccc667258a38d292b6466f160
gmccannon <gwm13@uakron.edu>
2024-07-23 20:13:38 -0400

    Change background element

--- front/src/App.js
+++ front/src/App.js
@@ -4,15 +4,14 @@ import MyButton from "./components/MyButton.js"
 import { useState } from 'react';
 
 function App() {
-  const [theme, setTheme] = useState(0);
-  const backgroundColor = theme === 0 ? 'white' : 'black';
+  const [theme, setTheme] = useState('Black');
 
   return (
-    <div style={ {backgroundColor} } className="App">
+    <div className="App">
       <header className="App-header">
         <MyButton theme={theme} setTheme={setTheme} />
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Upload a file to convert it to a pdf</h1>
+        <h1 style={{backgroundColor: theme}}>Upload a file to convert it to a pdf</h1>
         <form action="/uploader" method="post" enctype="multipart/form-data">
           <input type="file" name="file" required></input>
           <input type="submit" value="Upload"></input>
--- front/src/components/MyButton.js
+++ front/src/components/MyButton.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 
 function MyButton ({ theme, setTheme }) {
-
     return(
         <>
-            <button onClick={() => setTheme(theme == 1 ? 0 : 1)}>Click here</button>
+            <button onClick={() => setTheme(theme == 'Black' ? 'White' : 'Black')}>{theme === 0 ? 'Switch to Dark Theme' : 'Switch to Light Theme'}</button>
         </>
         
     )
