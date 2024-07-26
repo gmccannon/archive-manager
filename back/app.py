5d80ddaf35a05aa208983cfa3755f69484245ba3
gmccannon <gwm13@uakron.edu>
2024-07-26 11:10:14 -0400

    Add basic drag and drop

--- back/app.py
+++ back/app.py
@@ -1,6 +1,4 @@
-from flask import Flask, request, send_file, render_template
-import pandas as pd
-import os
+from flask import Flask, request, send_file
 from reportlab.pdfgen import canvas
 from flask_cors import CORS
 
--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
+import DragDrop from './components/DragDrop';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <h1>Upload a file to convert it to a pdf</h1>
-        <form action="/uploader" method="post" enctype="multipart/form-data">
-          <input type="file" name="file" required></input>
-          <input type="submit" value="Upload"></input>
-          <input type="reset" value="Clear File"></input>
-        </form>
+        <DragDrop />
       </header>
     </div>
   );
