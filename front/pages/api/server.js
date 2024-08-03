7e4b1e8944a6dbed2230d815f32e16ac4aa825f8
gmccannon <gwm13@uakron.edu>
2024-08-03 10:53:27 -0400

    Edit vercel json

--- front/pages/api/server.js
+++ front/pages/api/server.js
@@ -16,8 +16,8 @@ app.post('/decompressor', (req, res) => {
 
 // Add error handling middleware
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
-  });
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
 
 export const handler = serverless(app);
