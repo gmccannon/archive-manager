7e384736b19a45e5d068e8fc5a2773431894d8f5
gmccannon <gwm13@uakron.edu>
2024-08-03 10:06:32 -0400

    Add error handling middleware

--- front/pages/api/server.js
+++ front/pages/api/server.js
@@ -14,4 +14,10 @@ app.post('/decompressor', (req, res) => {
   res.send('Hello from Express decompress!');
 });
 
+// Add error handling middleware
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+  });
+
 export const handler = serverless(app);
